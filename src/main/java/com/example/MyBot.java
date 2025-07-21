b0c5146b4ef766c3520a50b6369160295201cb2f
brin304ka <brin304ka@gmail.com>
2025-07-21 23:32:09 +0300

    второй комит кнопки плюс много результатов проблема с рецептом Battenberg Cake код ошибки 400

--- src/main/java/com/example/MyBot.java
+++ src/main/java/com/example/MyBot.java
@@ -1,15 +1,16 @@
 package com.example;
 
-
-import java.net.URI;
-import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class MyBot extends TelegramLongPollingBot {
@@ -18,12 +19,12 @@ public class MyBot extends TelegramLongPollingBot {
 
     @Override
     public String getBotUsername(){
-        return "";
+        return "@recipesForMumbot";
     }
 
     @Override
     public String getBotToken(){
-        return "";
+        return "7605976168:AAFs63sOkXcvMeHDrJWaNS3We7xp1pmQWyk";
     }
 
     @Override
@@ -38,7 +39,11 @@ public class MyBot extends TelegramLongPollingBot {
             try {
                 if((!(session==null))&&!session.isFinished){
                     try {
-                        sendMessage(id, recipe.getRecipeText("https://www.themealdb.com/api/json/v1/1/search.php?s=" + messageText));
+                        ArrayList<Recipe> recipeList = new ArrayList<>();
+                        recipeList = recipe.getMultiplyRecipe("https://www.themealdb.com/api/json/v1/1/search.php?s=" + messageText);
+                        for (Recipe recipe2 : recipeList) {
+                            sendMessage(id, recipe2.getTitle() + "\n \n" + "Category: " + recipe2.getCategory() + "\n \n" + recipe2.getIngredients() + "\n \n" + recipe2.getInstruction());
+                        }
                     } catch (Exception e) {
                         e.printStackTrace();
                         sendMessage(id, "I couldn't find a recipe with that name. Try another one");
@@ -47,14 +52,14 @@ public class MyBot extends TelegramLongPollingBot {
                     return;
                 }
 
-                switch (messageText.toLowerCase()) {
+                switch (messageText) {
                     case "/start":
-                        sendMessage(id, "Hi! I'm recipe bot. Let's choose what we are going to cook today!");
+                        sendMessageWithKeyboard(id, "Hi! I'm recipe bot. Let's choose what we are going to cook today!");
                         break;
-                    case "/randomrecipe":
+                    case "Get random recipe":
                         responce = recipe.getRecipeText("https://www.themealdb.com/api/json/v1/1/random.php");
                         break;
-                    case "/recipebytitle":
+                    case "Find recipe by title":
                         session = new UserSession();
                         sessions.put(id, session);
                         sendMessage(id, "What recipe do you want to get?");
@@ -73,7 +78,6 @@ public class MyBot extends TelegramLongPollingBot {
 
     private void sendMessage(long id, String text) {
         SendMessage sendMessage = new SendMessage();
-        
         sendMessage.setChatId(id);
         sendMessage.setText(text);
         try{
@@ -83,10 +87,39 @@ public class MyBot extends TelegramLongPollingBot {
         }
     }
 
-    private void sendRecipe(long id,Recipe recipe, String url) throws Exception{
-        SendPhoto sendPhoto = new SendPhoto();
-        sendPhoto.setChatId(id);
-        sendPhoto.setCaption(recipe.getRecipeText(url));
+    private void sendMessageWithKeyboard(long chatId, String text) {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(text);
+
+        // Создаем клавиатуру
+        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+
+        // Создаем строки клавиатуры
+        List<KeyboardRow> keyboard = new ArrayList<>();
+
+        KeyboardRow row1 = new KeyboardRow();
+        row1.add(new KeyboardButton("Get random recipe"));
+        KeyboardRow row2 = new KeyboardRow();
+        row2.add(new KeyboardButton("Find recipe by title"));
+
+        keyboard.add(row1);
+        keyboard.add(row2);
+
+        keyboardMarkup.setKeyboard(keyboard);
         
+        // Можно отключить автоматическую очистку клавиатуры после нажатия
+        keyboardMarkup.setOneTimeKeyboard(true);
+        
+        // Можно оставить клавиатуру всегда видимой или скрывать после выбора
+        keyboardMarkup.setResizeKeyboard(false);
+
+        message.setReplyMarkup(keyboardMarkup);
+
+        try {
+            execute(message);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/com/example/Recipe.java
+++ src/main/java/com/example/Recipe.java
@@ -1,6 +1,13 @@
 package com.example;
 
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import lombok.AllArgsConstructor;
@@ -40,4 +47,71 @@ public class Recipe {
         return recipe.getTitle() + "\n \n" + "Категория: " + recipe.getCategory() + "\n \n" + recipe.getIngredients() + "\n \n" + recipe.getInstruction();
     }
 
+    public ArrayList<Recipe> getMultiplyRecipe(String url1) throws Exception{
+        ArrayList<Recipe> recipeList = new ArrayList<>();
+        try {
+            // Создаем соединение
+            URL url = new URL(url1);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+
+            // Проверяем статус ответа
+            int responseCode = conn.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                // Читаем ответ
+                BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String inputLine;
+                StringBuilder responseContent = new StringBuilder();
+
+                while ((inputLine = in.readLine()) != null) {
+                    responseContent.append(inputLine);
+                }
+                in.close();
+
+                // Парсим JSON
+                JSONObject jsonObject = new JSONObject(responseContent.toString());
+
+                // Получаем массив "meals"
+                JSONArray mealsArray = jsonObject.optJSONArray("meals");
+
+                if (mealsArray != null) {
+                    for (int i = 0; i < mealsArray.length(); i++) {
+                        JSONObject object = mealsArray.getJSONObject(i);
+                        Recipe recipe = new Recipe();
+                        recipe.setTitle(object.getString("strMeal"));
+                        recipe.setCategory(object.getString("strCategory"));
+
+                        String ingredients ="";
+                        int a = 1;
+                        while(!(object.getString("strIngredient" + a).equalsIgnoreCase(""))){
+                            ingredients = ingredients + (object.getString("strIngredient" + a) + ": " +  object.getString("strMeasure" + a) + "\n");
+                            a++;
+                            if(a>20){
+                                break;
+                            }
+                        }
+                        recipe.setIngredients(ingredients);
+        
+                        recipe.setInstruction(object.getString("strInstructions"));
+
+                        recipe.setPhotoURL(object.getString("strMealThumb"));
+                        System.out.println(recipe.getTitle() + "\n \n" + "Категория: " + recipe.getCategory() + "\n \n" + recipe.getIngredients() + "\n \n" + recipe.getInstruction());
+                        recipeList.add(recipe);
+                    }
+                } else {
+                    return null;
+                }
+            } else {
+                System.out.println("Ошибка при выполнении запроса. Код ответа: " + responseCode);
+                return null;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        return recipeList;
+    }
+
+    
 }
